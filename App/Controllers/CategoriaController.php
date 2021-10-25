7045231c8e0bfa4c7e01a18df1e2402e2de731ed
Cristian Antony <cristian.buendia.m113@gmail.com>
2021-10-24 23:48:53 -0500

    delete1

--- App/Controllers/CategoriaController.php
+++ App/Controllers/CategoriaController.php
@@ -20,6 +20,7 @@ class CategoriaController extends Controller{
 
     public function insert(Request $req){
         $categoria = new Categoria();
+        $categoria = json_encode($categoria);
         $categoria->nombre = $req->getBody()->nombre;
         $categoria->descripcion = $req->getBody()->descripcion;
         $categoria->save();
